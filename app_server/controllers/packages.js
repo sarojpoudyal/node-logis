dd549695adb2a2a7d8b745ffe42702fe6b52a0f6
Saroj Sharma <saroj.webdeveloper@gmail.com>
2016-06-03 11:41:52 +0530

    initial views

--- app_server/controllers/packages.js
+++ app_server/controllers/packages.js
@@ -1,8 +1,18 @@
 module.exports.new = function(req, res, next){
-  res.render('createPackage', {title: 'Post new Package'});
+  res.render('createPackage', {title: "Logis - Paperless Transactions",  pageHeader: "New Package"});
 };
 module.exports.display = function(req, res, next){
-  res.render('show', {title: 'Transaction Details'});
+  res.render('show', {title: "Logis - Paperless Transactions" , pageHeader:'Transaction Details',vendor: 'Ramesh Agrawal',
+transactionDetails:{
+  id:'234567845',
+  initialDateDispatched: '23/3/2014',
+  initialAddress: 'Kathiar, Kolkata Nagar, West Bengal, 783453',
+  recciverAddress: 'Rangpo, Sikkim, 737129',
+  currentDateDispatched: '25/3/2014',
+  currentSender: 'Malda, West Bengal, 734534',
+  currentRecciver: 'Hill Cart Road, Siliguri, West Bengal, 737243'
+}
+});
 };
 module.exports.trackPackage = function(req, res, next){
   res.render('index', {title: "Track Package"});
--- app_server/controllers/vendors.js
+++ app_server/controllers/vendors.js
@@ -1,3 +1,55 @@
 module.exports.dashboard = function(req, res){
-  res.render('dashboard', {title: "Dashboard"});
+  res.render('dashboard', {title: "Logis - Paperless Transactions", pageHeader: "Dashboard",
+  vendor: 'Ramesh Agrawal',
+  transactionIn: [{
+    id: '123456543',
+    address: 'Main Road, Bhaglapur, Patna, Bihar, 737133',
+    dateDispatched: '23/3/2016'
+  },{
+    id: '123456543',
+    address: 'Main Road, Bhaglapur, Patna, Bihar, 737133',
+    dateDispatched: '23/3/2016'
+  },{
+    id: '123456543',
+    address: 'Main Road, Bhaglapur, Patna, Bihar, 737133',
+    dateDispatched: '23/3/2016'
+  },{
+    id: '123456543',
+    address: 'Main Road, Bhaglapur, Patna, Bihar, 737133',
+    dateDispatched: '23/3/2016'
+  },{
+    id: '123456543',
+    address: 'Main Road, Bhaglapur, Patna, Bihar, 737133',
+    dateDispatched: '23/3/2016'
+  }],
+  transactionOut: [
+    {
+      id: '98765432',
+      address: 'Main Road, Jabalpur, Panipat, Bihar, 737333',
+      dateDispatched: '25/3/2016'
+    },
+    {
+      id: '98765432',
+      address: 'Main Road, Jabalpur, Panipat, Bihar, 737333',
+      dateDispatched: '25/3/2016'
+    },{
+      id: '98765432',
+      address: 'Main Road, Jabalpur, Panipat, Bihar, 737333',
+      dateDispatched: '25/3/2016'
+    },{
+      id: '98765432',
+      address: 'Main Road, Jabalpur, Panipat, Bihar, 737333',
+      dateDispatched: '25/3/2016'
+    },{
+      id: '98765432',
+      address: 'Main Road, Jabalpur, Panipat, Bihar, 737333',
+      dateDispatched: '25/3/2016'
+    },{
+      id: '98765432',
+      address: 'Main Road, Jabalpur, Panipat, Bihar, 737333',
+      dateDispatched: '25/3/2016'
+    }
+
+  ]
+  });
 };
